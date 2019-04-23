f7f331ac3f727eadac0b1e285e1eb317d3525c58
ballessay <code@ballessay.de>
2019-04-23 19:51:48 +0200

    Remove splashscreen

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "widgets/mainwindow.h"
 #include "tools/git2wrapper.h"
 #include "git2cpp/initializer.h"
-#include <QSplashScreen>
 #include <QString>
 #include <QMessageBox>
 
@@ -13,23 +12,13 @@ int main(int argc, char** argv)
 
   CApplication app(argc, argv);
 
-  QPixmap pixmap(":/quokka.jpg");
-  QSplashScreen splash(pixmap);
-  splash.show();
-  app.processEvents();
-
   try
   {
-    splash.showMessage("Init git...");
     CGit2Wrapper git(app.InitialRepoPath());
     git.Initialize();
 
     CMainWindow w(git);
     w.show();
-
-    splash.showMessage("Init GUI...");
-
-    splash.finish(&w);
 
     return app.exec();
   }
